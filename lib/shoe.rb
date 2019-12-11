7c29dbd2f73cb7ff47866a27a0350493188be1f9
Stela Capsa <stela.capsa@gmail.com>
2019-12-11 17:24:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe 
+  attr_accessor :color, :size, :material, :condition, :cobble
+  def initialize(brand = "Adiddas", condition="new")
+   @brand = brand 
+   @condition = condition
+  end
+  
+  def brand
+    @brand
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+   end
+Shoe.new
+
+ 
+end 
